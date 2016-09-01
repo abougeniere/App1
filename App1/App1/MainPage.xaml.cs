09a31549dfcf150f130084f71bb3fb620bc039a2
alain.bougeniere <abougeniere@yahoo;fr>
2016-09-01 15:15:31 +0200

    Ajoutez des fichiers projet.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void OnBouttonClicked(object sender, EventArgs args)
+        {
+            count++;
+
+            ((Button)sender).Text =
+                String.Format("{0} click{1}!", count, count == 1 ? "" : "s");
+        }
+    }
+}
